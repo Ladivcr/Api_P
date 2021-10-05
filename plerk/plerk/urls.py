fb43d7e2e1fee5a2be19977b1cde94203a7b90ae
ladivcr <vrosas832@gmail.com>
2021-10-05 17:52:53 -0500

    cambios, no funciona

--- plerk/plerk/urls.py
+++ plerk/plerk/urls.py
@@ -1,9 +1,12 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from companies.views import CompaniesViewSet
 from transactions.views import TransactionsViewSet
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 router = DefaultRouter()
 router.register(r'companies', CompaniesViewSet)
@@ -13,4 +16,5 @@ urlpatterns = router.urls
 
 urlpatterns += [
     path('admin/', admin.site.urls),
-]
+    path('api/', include('post.urls'))
+] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
